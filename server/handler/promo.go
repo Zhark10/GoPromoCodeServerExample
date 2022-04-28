bdad34848560667772030cc989bfee7b33599cbe
Zhark10 <verygoodman18@yandex.ru>
2022-04-28 13:10:51 +0300

    new package

--- server/handler/promo.go
+++ server/handler/promo.go
@@ -1,6 +1,8 @@
-package main
+package handler
 
-var PromoDict = map[productId]string{
+type promoId = string
+
+var PromoDict = map[promoId]string{
 	"black_friday": "X12F3R",
 	"big_sale":     "X12F1Y",
 	"birthday":     "X12G9S",
