7f1db2e23589b5365d632de7a44aa03f219d9efd
Zhark10 <verygoodman18@yandex.ru>
2022-04-28 12:47:57 +0300

    connection handler

---
+++ server/connection_handle.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func NewConnectionHandle(connection net.Conn) {
+	defer connection.Close()
+
+	for {
+		data := make([]byte, 1024*2)
+		size, readError := connection.Read(data)
+
+		if size == 0 || readError != nil {
+			fmt.Errorf("Read err: %s\n", readError)
+			break
+		}
+
+		productIdToSearch := data[:size]
+		target, ok := PromoDict[string(productIdToSearch)]
+		message := "unknown"
+		if ok {
+			message = target
+		}
+		connection.Write([]byte(message))
+	}
+}
